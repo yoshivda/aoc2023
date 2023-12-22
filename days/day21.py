f5fc7203bc7ac378be76059a4d048e5f895dbb1a
Yoshi van den Akker <yoshi.vanden.akker@cgi.com>
2023-12-22 14:45:52 +0100

    Add day 21 pt2

--- days/day21.py
+++ days/day21.py
@@ -1,9 +1,12 @@
+from math import floor
+
 from lib import load_input
+import numpy as np
 
 
 def solve(data):
-    return part_one(data.splitlines())
-    # return part_two(data.splitlines())
+    # return part_one(data.splitlines())
+    return part_two(data.splitlines())
 
 
 directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
@@ -21,7 +24,19 @@ def part_one(data):
 
 
 def part_two(data):
-    pass
+    reached = dict()
+    todo = {next((x, y) for x in range(len(data[0])) for y in range(len(data)) if data[y][x] == 'S')}
+    xs = []
+    ys = []
+    for i in range(65 + 131 * 3):
+        reached |= {coords: i for coords in todo}
+        todo = {(x + dx, y + dy) for dx, dy in directions for x, y in todo
+                if data[(y + dy) % len(data)][(x + dx) % len(data[0])] != '#'
+                and (x + dx, y + dy) not in reached}
+        if i % 131 == 65:
+            xs.append(i // 131)
+            ys.append(sum(v % 2 == i % 2 for v in reached.values()))
+    return round(np.polyval(np.polyfit(xs, ys, 2), 202300))
 
 
 if __name__ == '__main__':
